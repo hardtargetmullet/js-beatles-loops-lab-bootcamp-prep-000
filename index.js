127f25a0c4c17177ba81d860f621ae44f7541381
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 14:03:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function theBeatlesPlay(name, instrument) {
   
   return newArray
 }
+
+function johnLennonFacts(array) {
+  var newArray = []
+  
+  let i = 0;
+  while(i < array.length) {
+    newArray.push(array[i] + "!!!")
+    i++
+  }
+}
