da1d93caa61bc8222158c8abe3abed50124f4aec
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 14:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(n) {
   var newArray = []
   
-  
+  do {
+    newArray.push("I love the Beatles!")
+    n++
+  } while(n < 15);
   return newArray
 }
